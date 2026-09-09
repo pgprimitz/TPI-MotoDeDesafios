5154c2cebb4de5857cf1af61a4c5681d2ae87492
412023-OSELLA <412023@tecnicatura.frc.utn.edu.ar>
2026-09-09 12:21:29 -0300

    backend init

---
+++ challenge-engine/src/main/java/ar/edu/utn/frc/challenge_engine/ChallengeEngineApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.challenge_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeEngineApplication.class, args);
+	}
+
+}
---
+++ challenge-engine/src/test/java/ar/edu/utn/frc/challenge_engine/ChallengeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.challenge_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
